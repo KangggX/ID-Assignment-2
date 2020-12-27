82213429ea688710ec9e32de43bd056db6504fb8
KangggX <s10205347@connect.np.edu.sg>
2020-12-27 17:08:01 +0800

    tons of changes made to navbar

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 $(document).scroll(function () {
     var nav = $(".main .navbar");
+
     nav.toggleClass("bg-light shadow", $(this).scrollTop() > nav.height());
 
     if ($(this).scrollTop() > nav.height()) {
-        $(".navbar .container").removeClass("py-3").addClass("py-1");
+        $(".navbar .container").removeClass("py-3");
+        $(".nav-link, .navbar-brand").css("color", "rgba(0, 0, 0, .9)")
     } else {
-        $(".navbar .container").removeClass("py-1").addClass("py-3");
+        $(".navbar .container").addClass("py-3");
+        $(".nav-link, .navbar-brand").css("color", "")
     }
 });
