2ede04ecad3a212913d847681546896019895350
KangggX <s10205347@connect.np.edu.sg>
2020-12-30 01:20:05 +0800

    more code added regarding api

--- script.js
+++ script.js
@@ -19,6 +19,7 @@ $("#querysubmit").click(function(e) {
     var qType = $("#querytype").val();
     var qDiet = $("#querydiet").val();
     var qIntolerance = $("#queryintolerance").val();
+    var idHolder =[];
 
     if ($("#querytime").val() == "") { //In order to prevent error 404 from happening if value is empty
         qTime = "";
@@ -39,16 +40,34 @@ $("#querysubmit").click(function(e) {
             },
         };
 
-        $.ajax(searchRecipeSettings).done(function (response) {
-            console.log(response);
+        var recipeInformation = {
+            "url": "https://api.spoonacular.com/recipes/652061/information?apiKey=a863c4b81ffa46ce882d936d36181f86",
+            "method": "GET",
+            "timeout": 0,
+            "headers": {
+              "Cookie": "__cfduid=d2164e84c3a575422c310609a8f51a4c31608527722"
+            },
+        };
+
+        $.ajax(searchRecipeSettings).done(function (recipe) {
+            console.log(recipe);
             $(".result-body").empty(); //Clear all contents when user click submit
-            for (var i = 0; i < response.results.length; i++) {
+            for (var i = 0; i < recipe.results.length; i++) {
+                idHolder.push(recipe.results[i].id); //Obtain all the recipe IDs
+
                 $(".result-body").append(
-                    `<div class="d-flex mb-5">
-                        <img class="img-fluid" src="${response.results[i].image}">${response.results[i].id}
+                    `<div id="data-${i}" class="d-flex flex-column align-items-center mb-5">
+                        <img class="img-fluid" src="${recipe.results[i].image}"><span class="fs-4 fw-bold">${recipe.results[i].title}</span>
                     </div>`
                 );
-            }
+                
+                $(`#data-${i}`).click(function(e) {
+                    $.ajax(recipeInformation).done(function (response) {
+                        console.log(response);
+                        console.log(idHolder);
+                    });
+                });
+            };
             
         });
     }
