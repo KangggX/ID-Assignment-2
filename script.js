3622ebe288cb3134630a88dad22bebbcc3de80e3
KangggX <s10205347@connect.np.edu.sg>
2020-12-29 14:42:45 +0800

    working on API portion

--- script.js
+++ script.js
@@ -11,3 +11,30 @@ $(document).scroll(function () {
         $(".nav-link, .navbar-brand").css("color", "")
     }
 });
+
+$("#querysubmit").click(function(e) {
+    var qName = $("#queryname").val();
+    var qTime = $("#querytime").val();
+    var qCuisine = $("#querycuisine").val();
+    var qType = $("#querytype").val();
+    var qDiet = $("#querydiet").val();
+    var qIntolerance = $("#queryintolerance").val();
+    console.log(qTime);
+    e.preventDefault();
+    if (qName.length == 0 && qCuisine.length == 0) {
+        console.log("Error");
+    } else {
+        //var settings = {
+        //    "url": `https://api.spoonacular.com/recipes/complexSearch?apiKey=a863c4b81ffa46ce882d936d36181f86&number=35&instructionsRequired=true&query=${qName}&cuisine=${qCuisine}&diet=${qDiet}&intolerances=${qIntolerance}&type=${qType}&maxReadyTime=${qTime}`,
+        //    "method": "GET",
+        //    "timeout": 0,
+        //    "headers": {
+        //      "Cookie": "__cfduid=da7f6af872b2cb304cab1d7e82c8db5021609153837"
+        //    },
+        //};
+
+        //$.ajax(settings).done(function (response) {
+        //    console.log(response);
+        //});
+    }
+})
