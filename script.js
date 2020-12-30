464199ab61241815abdff327b3b0810687e92f05
KangggX <s10205347@connect.np.edu.sg>
2020-12-30 14:28:04 +0800

    getting index position now works flawlessly

--- script.js
+++ script.js
@@ -32,20 +32,11 @@ $("#querysubmit").click(function(e) {
         console.log("Error");
     } else {
         var searchRecipeSettings = {
-            "url": `https://api.spoonacular.com/recipes/complexSearch?apiKey=a863c4b81ffa46ce882d936d36181f86&number=35&instructionsRequired=true&query=${qName}&cuisine=${qCuisine}&diet=${qDiet}&intolerances=${qIntolerance}&type=${qType}${qTime}`,
+            "url": `https://api.spoonacular.com/recipes/complexSearch?apiKey=a863c4b81ffa46ce882d936d36181f86&number=50&instructionsRequired=true&query=${qName}&cuisine=${qCuisine}&diet=${qDiet}&intolerances=${qIntolerance}&type=${qType}${qTime}`,
             "method": "GET",
             "timeout": 0,
             "headers": {
               "Cookie": "__cfduid=da7f6af872b2cb304cab1d7e82c8db5021609153837"
-            },
-        };
-
-        var recipeInformation = {
-            "url": "https://api.spoonacular.com/recipes/652061/information?apiKey=a863c4b81ffa46ce882d936d36181f86",
-            "method": "GET",
-            "timeout": 0,
-            "headers": {
-              "Cookie": "__cfduid=d2164e84c3a575422c310609a8f51a4c31608527722"
             },
         };
 
@@ -56,18 +47,29 @@ $("#querysubmit").click(function(e) {
                 idHolder.push(recipe.results[i].id); //Obtain all the recipe IDs
 
                 $(".result-body").append(
-                    `<div id="data-${i}" class="d-flex flex-column align-items-center mb-5">
-                        <img class="img-fluid" src="${recipe.results[i].image}"><span class="fs-4 fw-bold">${recipe.results[i].title}</span>
+                    `<div id="data-${i}" class="d-flex flex-column align-items-center mb-5" style="padding: 0 5rem;">
+                        <img class="img-fluid" src="${recipe.results[i].image}"><span class="fs-5 fw-bold">${recipe.results[i].title}</span>
                     </div>`
                 );
                 
-                $(`#data-${i}`).click(function(e) {
-                    $.ajax(recipeInformation).done(function (response) {
-                        console.log(response);
-                        console.log(idHolder);
-                    });
-                });
+            
             };
+
+            $(".result-body div").click(function(e) {
+                var idIndex = $(".result-body div").index(this); //Get the index position of the item clicked
+                var recipeInformation = {
+                    "url": `https://api.spoonacular.com/recipes/${idHolder[idIndex]}/information?apiKey=a863c4b81ffa46ce882d936d36181f86`,
+                    "method": "GET",
+                    "timeout": 0,
+                    "headers": {
+                      "Cookie": "__cfduid=d2164e84c3a575422c310609a8f51a4c31608527722"
+                    },
+                };
+
+                $.ajax(recipeInformation).done(function (response) {
+                    console.log(response);    
+                });
+            });
             
         });
     }
